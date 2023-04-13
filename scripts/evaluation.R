a0ba65ed6420fef8f638b05b78d84bae58f95b5e
Camila Natalia Barragan Ibañez <c.n.barraganibanez@uu.nl>
2023-04-13 23:35:13 +0200

    Fix evaluation function
    - I  added which data set is used to calculate the proportion of Bayes factors that are larger than the threshold.
    - I added the evaluation of the condition.

--- scripts/evaluation.R
+++ scripts/evaluation.R
@@ -3,12 +3,14 @@
 
 # Evaluation of threshold
 
-eval.thresh <- function(results, BF.thresh = BF.thresh, n.datasets = n.datasets){
+eval.thresh <- function(results.H0 = results.H0, results.H1 = results.H1, BF.thresh = BF.thresh, n.datasets = n.datasets, condition = condition, eta = eta){
     # Proportion
-    prop.BF12 <- length(which(results[, 'BF.12'] > BF.thresh)) / n.datasets #Or 01 instead?
-    prop.BF21 <- length(which(results[, 'BF.21'] > BF.thresh)) / n.datasets #Or 10?
-    
+    prop.BF12 <- length(which(results.H0[, 'BF.12'] > BF.thresh)) / n.datasets #Or 01 instead? Proportion of BF12 when H0 = TRUE
+    prop.BF21 <- length(which(results.H1[, 'BF.21'] > BF.thresh)) / n.datasets #Or 10? Proportion of BF21 when H1 = TRUE
+    # Evaluation
+    ifelse(prop.BF12 > eta & prop.BF21 > eta, condition <- TRUE, condition <- FALSE)
     #Output
-    return(list(prop.BF12 = prop.BF12,
-                prop.BF21 = prop.BF21))
+    return(condition)
 }
+
+#Test --------------------------------------------------------------------------------
