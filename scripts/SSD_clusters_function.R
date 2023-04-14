c8180d0c875865bf4cf71fa6088ea05c59261af2
Camila Natalia Barragan Ibañez <c.n.barraganibanez@uu.nl>
2023-04-14 12:05:57 +0200

    Added conditionals for increasing sample
    I have added n1.fixed and n2.fixed arguments and included the conditions for when one of them is fixed.

--- scripts/SSD_clusters_function.R
+++ scripts/SSD_clusters_function.R
@@ -12,7 +12,9 @@
 ## hypothesis: The hypothesis that is going to be tested.
 
 
-SSD_crt <- function(eff.size, n1 = 15, n2 = 30, n.datasets = 100, rho, BF.thresh, eta = 0.8, plots = TRUE, hypothesis) {
+SSD_crt <- function(eff.size, n1 = 15, n2 = 30, n.datasets = 100, rho, BF.thresh, 
+                    eta = 0.8, plots = TRUE, hypothesis, n1.fixed = TRUE,
+                    n2.fixed = TRUE) {
     # Libraries
     library(lme4)
     library(bain)
@@ -34,22 +36,32 @@ SSD_crt <- function(eff.size, n1 = 15, n2 = 30, n.datasets = 100, rho, BF.thresh
     # Names of table with results
     names.table <- c('Dcontrol', 'Dtreatment', 'BF.12', 'BF.21', 'BFc.1', 'BFc.2', 'PMP.1',
                      'PMP.2', 'PMPc.1', 'PMPc.2', 'PMPc.c')
-    # while starts here
     condition <- FALSE #condition fulfillment indicator
-    # If H1 is true
-    data.H1 <- do.call(gen_CRT_data, list(n.datasets, n1, n2, var.u0, var.e, rho, eff.size))
-    results.H1 <- do.call(extract_results, list(n.datasets, data.H1, names.table = names.table))
     
-    
-    # If H0 is true
-    eff.size <- 0
-    data.H0 <- do.call(gen_CRT_data, list(n.datasets, n1, n2, var.u0, var.e, rho, eff.size))
-    results.H0 <-  do.call(extract_results, list(n.datasets, data.H0, names.table = names.table))
-    
-    #Evaluation of condition
-    condition <- eval.thresh(results.H0 = results.H0, results.H1 = results.H1, 
-                             BF.thresh = BF.thresh, n.datasets = n.datasets, condition = condition, eta = eta)
-    
+    while (condition == FALSE) {
+        # If H1 is true
+        data.H1 <- do.call(gen_CRT_data, list(n.datasets, n1, n2, var.u0, var.e, rho, eff.size))
+        results.H1 <- do.call(extract_results, list(n.datasets, data.H1, names.table = names.table))
+        
+        # If H0 is true
+        eff.size <- 0
+        data.H0 <- do.call(gen_CRT_data, list(n.datasets, n1, n2, var.u0, var.e, rho, eff.size))
+        results.H0 <-  do.call(extract_results, list(n.datasets, data.H0, names.table = names.table))
+        
+        #Evaluation of condition
+        condition <- eval_thresh(results.H0 = results.H0, results.H1 = results.H1, 
+                                 BF.thresh = BF.thresh, n.datasets = n.datasets, condition = condition, eta = eta)
+        
+        # If condition =FALSE then increase the sample. Which n increase?
+        if (condition == FALSE){
+            if (n1.fixed == TRUE) {
+                n2 = n2 + 2
+            } else (n2.fixed == TRUE){
+                n1 = n1 + 1
+            }
+        }
+        
+    }
     
     # output
     #medians.results <- apply(results, 2, median) #returns a vector for plots
@@ -60,12 +72,17 @@ SSD_crt <- function(eff.size, n1 = 15, n2 = 30, n.datasets = 100, rho, BF.thresh
 
 #TODO:
     # - Add ICC calculation
-    # - Add n1.fixed = TRUE and n2.fixed = TRUE as arguments.
-    # - If n1.fixed = TRUE then increase n2. If n2.fixed = TRUE then increase n1.
-    # - Add while and increase n1 or n2.
     # - Think: If n1.fixed and n2.fixed are FALSE, then what?
     # - Think: How is going to be the output?
     # - Think: Should I change BF12 to BF01 and BF21 to BF10?
     # - Check style with lintR
     # - Talk w/ Uli about the name of the arguments → They should be the same.
     # - Test evaluation function.
+    # - Think a better name for the condition.
+    # - Hypothesis should be entered by researcher. Change this in data_generation function.
+    # - Add plots.This could be a function.
+    # - Add binary search algorithm.
+
+# Warnings -------------------------------------------------------------------
+# Check that n2 is even.
+# Check that n1.fixed and n2.fixed aro not TRUE both.
