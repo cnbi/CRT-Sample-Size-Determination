76e74867d7cc1cc606d6062f25e8fd436a03c515
Camila Natalia Barragan Ibañez <c.n.barraganibanez@uu.nl>
2023-04-14 12:01:46 +0200

    Changed style
    Changed the function's name to tidyverse stile guide

--- scripts/evaluation.R
+++ scripts/evaluation.R
@@ -3,7 +3,7 @@
 
 # Evaluation of threshold
 
-eval.thresh <- function(results.H0 = results.H0, results.H1 = results.H1, BF.thresh = BF.thresh, n.datasets = n.datasets, condition = condition, eta = eta){
+eval_thresh <- function(results.H0 = results.H0, results.H1 = results.H1, BF.thresh = BF.thresh, n.datasets = n.datasets, condition = condition, eta = eta){
     # Proportion
     prop.BF12 <- length(which(results.H0[, 'BF.12'] > BF.thresh)) / n.datasets #Or 01 instead? Proportion of BF12 when H0 = TRUE
     prop.BF21 <- length(which(results.H1[, 'BF.21'] > BF.thresh)) / n.datasets #Or 10? Proportion of BF21 when H1 = TRUE
