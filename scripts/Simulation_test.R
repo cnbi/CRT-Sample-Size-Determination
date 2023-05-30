9bfb5dfd0ecf5bc9dca2121192b689b977ca49db
Camila Natalia Barragan Ibañez <c.n.barraganibanez@uu.nl>
2023-05-30 15:44:18 +0200

    New factors and code
    I added two new factors: BF threshold and fix.
    I added the function and save results for every row.

--- scripts/Simulation_test.R
+++ scripts/Simulation_test.R
@@ -1,22 +1,33 @@
-############################### TEST #########################################
+###########################TEST INFORMATIVE HYPOTHESIS ############################
 
 # Design matrix
 n1 <- c(5, 10, 20, 40)
 n2 <- c(30, 60 ,90)
-rho <- c(0.25, 0.05, 0.1)
+rho <- c(0.25, 0.05, 0.1) #Intraclass correlation
 eff.size <- c(0.2, 0.5, 0.8)
-design.matrix <- expand.grid(n1, n2, rho, eff.size)
+bf.thresh <- c(1, 3, 5)
+fix <- c("n1", "n2")
+design.matrix <- expand.grid(n1, n2, rho, eff.size, bf.thresh)
 
 nrow.design <- nrow(design.matrix)
-iterations <- 10
+
 
 for (Row in seq(nrow.design)) {
-    for (i in seq(iterations)) {
-        
-    }
+    # function
+    ssd_results <- SSD_crt_inform(eff.size = design.matrix[Row, 4], 
+                                  n1 = design.matrix[Row, 1],
+                                  n2 = design.matrix[Row, 2],
+                                  n.datasets = 1000,
+                                  rho = design.matrix[Row, 3],
+                                  BF.thresh = design.matrix[Row, 5],
+                                  eta = 0.8, 
+                                  fixed = design.matrix[Row, 6])
+    # Save results
+    save(ssd_results, file = paste("ResultRow", Row, ".Rdata", sep = ""))
+
 }
 
-
+# Plots
 
 
 # - Add plots.This could be a function.
