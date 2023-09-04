73bba65901bfb1d0ef15256a06a8a183a8c98626
Camila Natalia Barragan Ibañez <c.n.barraganibanez@uu.nl>
2023-09-04 16:30:41 +0200

    Fix indentation

--- scripts/data_generation.R
+++ scripts/data_generation.R
@@ -102,18 +102,6 @@ gen_CRT_data <- function(n.datasets = n.datasets, n1 = n1, n2 = n2, var.u0 = var
     }
     
     # Multilevel analysis ---------------------------------------------------------
-    # output.lmer <- lmer(resp ~ Dintervention + Dcontrol - 1 + (1 | ID), data = data)
-    # estimates <- fixef(output.lmer) #means
-    # cov.intervention <- matrix(vcov(output.lmer)[1], nrow = 1, ncol = 1) #variance-covariance matrix
-    # cov.control <- matrix(vcov(output.lmer)[4], nrow = 1, ncol = 1)
-    # cov.list <- list(cov.intervention, cov.control)
-    # variances <- as.data.frame(VarCorr(output.lmer))
-    # var.u0.data <- variances[1, 4]
-    # var.e.data <- variances[2, 4]
-    # total.var.data <- var.u0.data + var.e.data
-    # rho.data <- var.u0.data / total.var.data
-    # ifelse(isSingular(output.lmer), marker <- 1, marker <- 0)
-    
     output.lmer <- lapply(data.list, lmer, resp ~ Dintervention + Dcontrol - 1 + (1 | ID), data = data) # I'm not sure if the equation should be a string
     estimates <- lapply(output.lmer, fixef)                 # Means
     cov.intervention <- lapply(output.lmer, varcov, 1)
@@ -127,26 +115,24 @@ gen_CRT_data <- function(n.datasets = n.datasets, n1 = n1, n2 = n2, var.u0 = var
     
     #Approximated adjusted fractional Bayes factors 
     n.eff <- ((n1 * n2) / (1 + (n1 - 1) * rho.data))/2
-        
-        
-        # bain ------------------------------------------------------------------------
-        n.eff <- ((n1 * n2) / (1 + (n1 - 1) * rho.data))/2
-        output.bain <- bain(estimates, hypothesis = hypoth, 
-                            n = c(n.eff, n.eff), group_parameters = 1, Sigma = cov.list, 
-                            joint_parameters = 0, fraction = b)
-        
-        # Results ---------------------------------------------------------------------
-        lmer.list[[i]] <- output.lmer
-        bain.list[[i]] <- output.bain
-        print(c(i, n1, n2)) #change this
-        #browser()
-        results[i, 1] <- output.bain$estimates[2] # Coefficient control
-        results[i, 2] <- output.bain$estimates[1] # Coefficient intervention
-        results[i, 3] <- output.bain$BFmatrix[1, 2] # Bayes factor H1vsH2 or H1vsH0
-        results[i, 4] <- output.bain$BFmatrix[2, 1] # Bayes factor H2vsH1 or H0vsH1
-        results[i, 5] <- output.bain$fit$PMPa[1] #posterior model probabilities of H1.
-        results[i, 6] <- output.bain$fit$PMPa[2] #posterior model probabilities of H2 or H0
-        results[i, 7] <- marker
+    
+    
+    output.bain <- bain(estimates, hypothesis = hypoth, 
+                        n = c(n.eff, n.eff), group_parameters = 1, Sigma = cov.list, 
+                        joint_parameters = 0, fraction = b)
+    
+    # Results ---------------------------------------------------------------------
+    lmer.list[[i]] <- output.lmer
+    bain.list[[i]] <- output.bain
+    print(c(i, n1, n2)) #change this
+    #browser()
+    results[i, 1] <- output.bain$estimates[2] # Coefficient control
+    results[i, 2] <- output.bain$estimates[1] # Coefficient intervention
+    results[i, 3] <- output.bain$BFmatrix[1, 2] # Bayes factor H1vsH2 or H1vsH0
+    results[i, 4] <- output.bain$BFmatrix[2, 1] # Bayes factor H2vsH1 or H0vsH1
+    results[i, 5] <- output.bain$fit$PMPa[1] #posterior model probabilities of H1.
+    results[i, 6] <- output.bain$fit$PMPa[2] #posterior model probabilities of H2 or H0
+    results[i, 7] <- marker
     
     return(output = results)
 }
