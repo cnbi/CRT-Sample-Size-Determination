a7d4feabeafc7498bae0fba2c396f9d38a71ed3f
Camila <cnbarragani@unal.edu.co>
2023-10-28 23:40:09 +0200

    Fix function to make plots with every increase

--- scripts/SSD_clusters_function_pl.R
+++ scripts/SSD_clusters_function_pl.R
@@ -46,16 +46,16 @@ SSD_crt_null_plots <- function(eff_size, n1 = 15, n2 = 30, ndatasets = 1000, rho
     null <- "Intervention=Control"
     # hypoth <- paste(null, ";", hypothesis1)
     
-    final_SSD <- vector(mode = "list", length = b_fract)
+    final_SSD <- vector(mode = "list", length = high)
     # Simulation and evaluation of condition  ----------------------------------
     for (b in seq(b_fract)) {
         previous <- 0
         previous_high <- 0
         high <- 350
-        while (best_result == FALSE) {
+        while (n2 < high + 1) {
             # If H1 is true
             data_H1 <- do.call(gen_CRT_data, list(ndatasets, n1, n2, var_u0, var_e,
-                                                  mean_interv = eff_size, b, 
+                                                  mean_interv = eff_size, b,
                                                   type = "equality"))
             colnames(data_H1) <- c("BF.10", "BF.01",
                                    "PMP.0", "PMP.1")
@@ -69,102 +69,108 @@ SSD_crt_null_plots <- function(eff_size, n1 = 15, n2 = 30, ndatasets = 1000, rho
             # Proportion
             prop_BF01 <- length(which(data_H0[, "BF.01"] > BF_thresh)) / ndatasets
             prop_BF10 <- length(which(data_H1[, "BF.10"] > BF_thresh)) / ndatasets
+            # save results
+            iteration <- iteration + 1
+            SSD_object[[iteration]] <- list("n1" = n1,
+                               "n2" = n2,
+                               "Proportion.BF01" = prop_BF01,
+                               "Proportion.BF10" = prop_BF10,
+                               "b.frac" = b,
+                               "data_H0" = data_H0,
+                               "data_H1" = data_H1)
+            # Increase
+            if (fixed = "n1") {
+                #Increase number of clusters
+                n2 = n2 + 2
+            } else if (fixed = "n2") {
+              n1 = n1 + 1
+            }
             # Evaluation
-            ifelse(prop_BF01 > eta & prop_BF10 > eta, condition_met <- TRUE, condition_met <- FALSE)
-            actual <- min(prop_BF10, prop_BF01)
+            # ifelse(prop_BF01 > eta & prop_BF10 > eta, condition_met <- TRUE, condition_met <- FALSE)
+            # actual <- min(prop_BF10, prop_BF01)
             # Binary search algorithm -----
-            if (condition_met == FALSE) {
-                print(c("Using cluster size:", n1, "and number of clusters:", n2,
-                        "prop_BF01: ", prop_BF01, "prop_BF10: ", prop_BF10, "b:", b))
-                if (fixed == "n1") {      # We need to increase the sample size
-                    low <- n2                         #lower bound
-                    high <- high                      #higher bound
-                    n2 <- round((low + high) / 2)     #point in the middle
-                    ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
-                    if (low + n2 == high * 2){          #when there is a roof effect
-                        low <- n2
-                        high <- 1000
-                        n2 <- round((low + high) / 2)     #point in the middle
-                    }
-                } else if (fixed == "n2") { # We need to increase the sample size
-                    low <- n1                        #lower bound
-                    high <- high                     #higher bound
-                    n1 <- round((low + high) / 2)    #point in the middle
-                    if (low + n1 == high * 2){         #when there is a roof effect
-                        low <- n1
-                        high <- previous_high
-                        n1 <- round((low + high) / 2)    #point in the middle
-                    }
-                    
-                }
-            } else if (condition_met == TRUE) {
-                print(c("Using cluster size:", n1,
-                        "prop_BF01: ", prop_BF01, "prop_BF10: ", prop_BF10,
-                        "low: ", low, "n2: ", n2, "high: ", high, "b:", b))
-                previous_high <- high
-                if (fixed == "n1") {
-                    if (actual - eta < 0.1) { # Proportion is close enough
-                        best_result == TRUE
-                        break
-                    } else if (previous == actual) { #If there is no change in the proportion and the lower
-                        if (n2 - low == 2) {         #bound is close to the middle point.
-                            best_result == TRUE
-                            break
-                        } else {   # Decreasing to find the optimal sample size
-                            low <- low
-                            high <- n2
-                            n2 <- round((low + high) / 2)   #point in the middle
-                            ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
-                            if (n2 < 30) warning("The number of groups is less than 30.
-                                                 This could lead to problems in convergence and singularity.")
-                            print("Lowering") # Eliminate later
-                        }
-                    } else {       # Decreasing to find the optimal sample size
-                        low <- low
-                        high <- n2
-                        n2 <- round((low + high) / 2)    #point in the middle
-                        ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
-                        if (n2 < 30) warning("The number of groups is less than 30.
-                                             This could lead to problems in convergence and singularity.")
-                        print("Lowering") # Eliminate later
-                    }
-                } else if (fixed == "n2") {
-                    if (actual - eta < 0.1) { # Proportion close enough
-                        best_result == TRUE
-                        break
-                    } else if (actual == previous) { # There is no change in the proportion and the lower bound
-                        if (n1 - low == 1) {         #bound is close enough
-                            best_result == TRUE
-                            break
-                        } else {                # Decreasing to find the optimal
-                            low <- low
-                            high <- n1
-                            n1 <- round((low + high) / 2)   #point in the middle
-                            print("Lowering") # Eliminate later
-                        }
-                    } else {                    # Decreasing to find the optimal
-                        low <- low
-                        high <- n1
-                        n1 <- round((low + high) / 2)    #point in the middle
-                        print("Lowering") # Eliminate later
-                    }
-                }
-            }
-            previous <- min(prop_BF10, prop_BF01)
+            # if (condition_met == FALSE) {
+            #     print(c("Using cluster size:", n1, "and number of clusters:", n2,
+            #             "prop_BF01: ", prop_BF01, "prop_BF10: ", prop_BF10, "b:", b))
+            #     if (fixed == "n1") {      # We need to increase the sample size
+            #         low <- n2                         #lower bound
+            #         high <- high                      #higher bound
+            #         n2 <- round((low + high) / 2)     #point in the middle
+            #         ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
+            #         if (low + n2 == high * 2){          #when there is a roof effect
+            #             low <- n2
+            #             high <- 1000
+            #             n2 <- round((low + high) / 2)     #point in the middle
+            #         }
+            #     } else if (fixed == "n2") { # We need to increase the sample size
+            #         low <- n1                        #lower bound
+            #         high <- high                     #higher bound
+            #         n1 <- round((low + high) / 2)    #point in the middle
+            #         if (low + n1 == high * 2){         #when there is a roof effect
+            #             low <- n1
+            #             high <- previous_high
+            #             n1 <- round((low + high) / 2)    #point in the middle
+            #         }
+            # 
+            #     }
+            # } else if (condition_met == TRUE) {
+            #     print(c("Using cluster size:", n1,
+            #             "prop_BF01: ", prop_BF01, "prop_BF10: ", prop_BF10,
+            #             "low: ", low, "n2: ", n2, "high: ", high, "b:", b))
+            #     previous_high <- high
+            #     if (fixed == "n1") {
+            #         if (actual - eta < 0.1) { # Proportion is close enough
+            #             best_result == TRUE
+            #             break
+            #         } else if (previous == actual) { #If there is no change in the proportion and the lower
+            #             if (n2 - low == 2) {         #bound is close to the middle point.
+            #                 best_result == TRUE
+            #                 break
+            #             } else {   # Decreasing to find the optimal sample size
+            #                 low <- low
+            #                 high <- n2
+            #                 n2 <- round((low + high) / 2)   #point in the middle
+            #                 ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
+            #                 if (n2 < 30) warning("The number of groups is less than 30.
+            #                                      This could lead to problems in convergence and singularity.")
+            #                 print("Lowering") # Eliminate later
+            #             }
+            #         } else {       # Decreasing to find the optimal sample size
+            #             low <- low
+            #             high <- n2
+            #             n2 <- round((low + high) / 2)    #point in the middle
+            #             ifelse(n2 %% 2 == 0, n2 <- n2, n2 <- n2 + 1)
+            #             if (n2 < 30) warning("The number of groups is less than 30.
+            #                                  This could lead to problems in convergence and singularity.")
+            #             print("Lowering") # Eliminate later
+            #         }
+            #     } else if (fixed == "n2") {
+            #         if (actual - eta < 0.1) { # Proportion close enough
+            #             best_result == TRUE
+            #             break
+            #         } else if (actual == previous) { # There is no change in the proportion and the lower bound
+            #             if (n1 - low == 1) {         #bound is close enough
+            #                 best_result == TRUE
+            #                 break
+            #             } else {                # Decreasing to find the optimal
+            #                 low <- low
+            #                 high <- n1
+            #                 n1 <- round((low + high) / 2)   #point in the middle
+            #                 print("Lowering") # Eliminate later
+            #             }
+            #         } else {                    # Decreasing to find the optimal
+            #             low <- low
+            #             high <- n1
+            #             n1 <- round((low + high) / 2)    #point in the middle
+            #             print("Lowering") # Eliminate later
+            #         }
+            #     }
+            # }
             print(c("low:", low, "n2:", n2, "h:", high, "b:", b)) # Eliminate
-            if (n2 == 350) {
-                break
-            } else if (n1 == 350) {
-                break
-            }
         }
-        SSD_object <- list("n1" = n1,
-                           "n2" = n2,
-                           "Proportion.BF01" = prop_BF01,
-                           "Proportion.BF10" = prop_BF10,
-                           "b.frac" = b,
-                           "data_H0" = data_H0,
-                           "data_H1" = data_H1)
+        
+        print(c("Using cluster size: ", n1, "and number of clusters: ", n2,
+                "prop.BF01: ", prop.BF01, "prop.BF10: ", prop.BF10))
         final_SSD[[b]] <- SSD_object
     }
     final_SSD[[b_fract + 1]] <- list(null, hypothesis1)
